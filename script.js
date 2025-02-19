a8262a14d14c8662839cbd064166da3c1d5da80b
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-19 17:09:35 +0100

    Add basic operations and operate functions

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-// Functions for all 4 basic math operator
-    // add
-    // subtract
-    // multiply
-    // divide
-// Function for operation, taking 2 numbers and operator as args
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/': 
+            return divide(num1, num2);
+    }
+
+}
 // Function for populating display with clicked numbers
