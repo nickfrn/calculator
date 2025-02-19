10ddba0b7316b9ba27618e41322084c7587a5e14
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-19 14:14:57 +0100

    Add script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Functions for all 4 basic math operator
+    // add
+    // subtract
+    // multiply
+    // divide
+// Function for operation, taking 2 numbers and operator as args
+// Function for populating display with clicked numbers
