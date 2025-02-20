e43448f71b8c7f42e6b5a9f4dab1efe00906706e
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-20 18:45:38 +0100

    Add event listener with operation's logic

--- script.js
+++ script.js
@@ -14,10 +14,6 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-let firstNum;
-let secondNum;
-let operator;
-
 function operate(operator, num1, num2) {
     switch (operator) {
         case '+':
@@ -31,4 +27,47 @@ function operate(operator, num1, num2) {
     }
 
 }
-// Function for populating display with clicked numbers
+
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let result;
+
+const container = document.querySelector('.container');
+const display = document.querySelector('.display');
+
+display.textContent = '0';
+
+container.addEventListener('click', (event) => {
+    const numbers = '0123456789';
+    const operators = '+-*/';
+
+    if (operators.includes(event.target.textContent)) {
+        operator = '';
+        operator += event.target.textContent;
+    } 
+
+    if (numbers.includes(event.target.textContent) && operator.length == 0) {
+        firstNum += event.target.textContent;
+
+        display.textContent = firstNum;
+    } else if (numbers.includes(event.target.textContent) && operator.length == 1) {
+        secondNum += event.target.textContent;
+
+        display.textContent = secondNum;
+    }
+
+    if (event.target.textContent == '=' &&
+        firstNum.length > 0 &&
+        secondNum.length > 0 &&
+        operator.length == 1
+    ) {
+        result = operate(operator, +firstNum, +secondNum);
+
+        firstNum = result;
+        secondNum = '';
+        operator = '';
+
+        display.textContent = firstNum;
+    }
+})
